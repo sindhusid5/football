6d04fed0471542230d357a06e8a748d332fe8247
sindhusid5 <sindhuja.tq@gmail.com>
2024-02-28 02:45:32 -0500

    initial

---
+++ routes/player.js
@@ -0,0 +1,126 @@
+const fs = require('fs');
+
+module.exports = {
+    addPlayerPage: (req, res) => {
+        res.render('add-player.ejs', {
+            title: "Ninja Player | Add a new player",
+            message: ''
+        });
+    },
+    addPlayer: (req, res) => {
+        if (!req.files) {
+            return res.status(400).send("No files were uploaded.");
+        }
+
+        let message = '';
+        let firstName = req.body.first_name;
+        let lastName = req.body.last_name;
+        let position = req.body.position;
+        let number = req.body.number;
+        let username = req.body.username;
+        let ranking = req.body.ranking; 
+        let value = req.body.value;  
+
+        let uploadedFile = req.files.image;
+        let image_name = uploadedFile.name;
+        let fileExtension = uploadedFile.mimetype.split('/')[1];
+        image_name = username + '.' + fileExtension;
+
+        let usernameQuery = "SELECT * FROM `players` WHERE user_name = '" + username + "'";
+
+        db.query(usernameQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            if (result.length > 0) {
+                message = 'Username already exists';
+                res.render('add-player.ejs', {
+                    message,
+                    title: "Ninja Players | Add a new player"
+                });
+            } else {
+                // check the filetype before uploading it
+                if (uploadedFile.mimetype === 'image/png' || uploadedFile.mimetype === 'image/jpeg' || uploadedFile.mimetype === 'image/gif') {
+                    // upload the file to the /public/assets/img directory
+                    uploadedFile.mv(`public/assets/img/${image_name}`, (err) => {
+                        if (err) {
+                            return res.status(500).send(err);
+                        }
+                        // send the player's details to the database
+                        let query = "INSERT INTO `players` (first_name, last_name, position, number, image, user_name, ranking, value) VALUES ('" +
+                            firstName + "', '" + lastName + "', '" + position + "', '" + number + "', '" + image_name + "', '" + username + "', '" + ranking + "', '" + value + "')";
+                        db.query(query, (err, result) => {
+                            if (err) {
+                                return res.status(500).send(err);
+                            }
+                            res.redirect('/');
+                        });
+                    });
+                } else {
+                    message = "Invalid File format. Only 'gif', 'jpeg' and 'png' images are allowed.";
+                    res.render('add-player.ejs', {
+                        message,
+                        title: "Welcome to Socka | Add a new player"
+                    });
+                }
+            }
+        });
+    },
+    editPlayerPage: (req, res) => {
+        let playerId = req.params.id;
+        let query = "SELECT * FROM `players` WHERE id = '" + playerId + "' ";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.render('edit-player.ejs', {
+                title: "Edit Player",
+                player: result[0],
+                message: ''
+            });
+        });
+    },
+    editPlayer: (req, res) => {
+        let playerId = req.params.id;
+        let firstName = req.body.first_name;
+        let lastName = req.body.last_name;
+        let position = req.body.position;
+        let number = req.body.number;
+        let ranking = req.body.ranking;  
+        let value = req.body.value;  
+    
+        let query = "UPDATE `players` SET `first_name` = '" + firstName + "', `last_name` = '" + lastName + "', `position` = '" + position + "', `number` = '" + number + "', `ranking` = '" + ranking + "', `value` = '" + value + "' WHERE `players`.`id` = '" + playerId + "'";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.redirect('/');
+        });
+    },
+    
+    deletePlayer: (req, res) => {
+        let playerId = req.params.id;
+        let getImageQuery = 'SELECT image from `players` WHERE id = "' + playerId + '"';
+        let deleteUserQuery = 'DELETE FROM  players WHERE id = "' + playerId + '"';
+
+        db.query(getImageQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            let image = result[0].image;
+
+            fs.unlink(`public/assets/img/${image}`, (err) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                db.query(deleteUserQuery, (err, result) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    res.redirect('/');
+                });
+            });
+        });
+    }
+};
